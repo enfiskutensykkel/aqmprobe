d1c8fbb4f811453d59ab0f82c807f51ff15da4f1
Jonas Markussen <jonassm@ifi.uio.no>
2014-05-14 01:11:37 -0700

    Added script for rate control

---
+++ bottleneck.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+iface=$1
+rate=$2
+
+ifaces=(`ifconfig | grep -oE "(eth[0-9]+|wlan0)"`)
+
+for i in $ifaces __invalid__; do
+	if [ "$i" == "$iface" ]; then
+		break
+	fi
+done
+
+if [ "$i" == "__invalid__" ]; then
+	echo "Interface $iface not found"
+	exit 1
+fi
+
+if [ -z "$rate" ]; then
+	echo "Using default bandwidth"
+	rate=200kbit
+fi
+
+sysctl -w kernel.panic=20
+tc qdisc del dev $iface root
+tc qdisc add dev $iface root handle 1: htb default 10
+tc class add dev $iface parent 1: classid 1:10 htb rate $rate ceil $rate burst 1520
+tc qdisc add dev $iface parent 1:10 pfifo limit 10
